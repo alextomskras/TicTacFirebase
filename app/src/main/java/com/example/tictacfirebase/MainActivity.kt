3c13bb0d2c35b01c4a23f2ea415727f4d52230ca
fess <dreamer.alex.toms@gmail.com>
2026-04-09 19:03:53 +0300

    dsdssd

--- app/src/main/java/com/example/tictacfirebase/MainActivity.kt
+++ app/src/main/java/com/example/tictacfirebase/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.tictacfirebase
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import androidx.core.app.ActivityCompat
